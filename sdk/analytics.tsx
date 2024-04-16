6264e7e68821eb6c68e3b45fa94a2bba6e527bed
Daniel Moura <daniel-moura@outlook.com>
2024-04-15 23:36:09 -0300

    feat: add post_score on analytics

--- sdk/analytics.tsx
+++ sdk/analytics.tsx
@@ -1,6 +1,16 @@
 import type { AnalyticsEvent } from "apps/commerce/types.ts";
 
-export const sendEvent = <E extends AnalyticsEvent>(event: E) => {
+export interface PostScore {
+  name?: "post_score";
+  params?: {
+    productId?: string;
+    score?: number;
+  };
+}
+
+type CustomEvents = PostScore | AnalyticsEvent;
+
+export const sendEvent = <E extends CustomEvents>(event: E) => {
   console.log(JSON.stringify(event, null, 2));
   globalThis.window.DECO.events.dispatch(event);
 };
